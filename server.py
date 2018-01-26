dac49ed4209148120601a3a3a0397e030cce00fa
arkady <larytet@yahoo.com>
2018-01-26 19:38:03 +0200

    Add simply python server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 port=443
 print("Using port", port)
-httpd = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True, certfile='cert.pem')
 httpd.serve_forever()
 
