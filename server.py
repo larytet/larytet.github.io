589b5e3e1e449da64379e23779c3c08033089810
arkady <larytet@yahoo.com>
2018-01-26 19:54:27 +0200

    Add simply python server

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-import SimpleHTTPServer
 import ssl
+import http.server
 
+# HTTPRequestHandler class
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
 
-class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_my_headers()
-
-        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
-
-    def send_my_headers(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
-
-
-port=443
+port=8000
 print("Using port", port)
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port), HandlerClass=MyHTTPRequestHandler)
+httpd = http.server.HTTPServer(('0.0.0.0', port), RequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True, certfile='cert.pem')
 httpd.serve_forever()
 
