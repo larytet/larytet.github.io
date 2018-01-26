48f2f7183a8f80c986d9a05cb6b8ea70ae88f585
arkady <larytet@yahoo.com>
2018-01-26 19:55:32 +0200

    Add simply python server

--- server.py
+++ server.py
@@ -11,12 +11,12 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
         # Send message back to client
-        message = "Hello world!"
+        message = open("index.html", "r").read()
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
 
-port=8000
+port=443
 print("Using port", port)
 httpd = http.server.HTTPServer(('0.0.0.0', port), RequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True, certfile='cert.pem')
