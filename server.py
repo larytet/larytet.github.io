1908a093c2e741843b4a33e2b2e5b4348d8fc1b6
arkady <larytet@yahoo.com>
2018-01-26 19:09:23 +0200

    Add simply python server

---
+++ server.py
@@ -0,0 +1,8 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True,
+                                certfile='cert.pem')
+httpd.serve_forever()
+
---
+++ start-server.sh
@@ -0,0 +1,2 @@
+openssl req -new -x509 -keyout cert.pem -out cert.pem -days 365 -nodes
+
